7afead5219afdf103ad91e75695d14a160d80de0
Ishaan Dixit <ishaandixit21@gmail.com>
2022-12-16 19:54:36 +0530

    Added few lines of code

--- read.py
+++ read.py
@@ -1,5 +1,10 @@
 import cv2 as cv
-img = cv.imread('Photos/cat.jpg')
+# img = cv.imread('Photos/cat_large.jpg')
 
-cv.imshow('Cat', img)
-cv.waitKey(0)
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
+
+#Reading Videos
+
+capture = cv.VideoCapture('Videos/dog.mp4')
+
